fcb589588e1294f424a506a89e610e1de1a66c2a
Drew Mares <drewmmares@gmail.com>
2022-01-11 21:45:01 -0600

    feat: finish off app

--- main.py
+++ main.py
@@ -73,14 +73,3 @@ class PongApp(App):
 
 if __name__ == '__main__':
     PongApp().run()
-
-class PongApp(App):
-    def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
-
-
-if __name__ == '__main__':
-    PongApp().run()
